94a9aa6d4b96fb27e3eaa207b6762bf625b20ef7
John Rodler <john@jrod.io>
2015-01-02 15:33:38 -0500

    cleanup logic + fix user input bugs/glitches

--- app/js/es6/core.js
+++ app/js/es6/core.js
@@ -12,10 +12,7 @@
 // debug logger
 var local_debug = true;
 var log = function(s) {
-  if(local_debug) { console.log(s); }
-};
-log.error = function(s) {
-  if(local_debug) { console.error(s); }
+  if (local_debug) { console.log(s); }
 };
 
 // detect touch
@@ -63,19 +60,20 @@ class Simon extends Game {
   }
 
   // reset
-  init() { log('init()');
+  init() {
 
     // reset & start sequence
     this._level         = 1;
     this._timer         = [];
     this._moves         = [];
     this._cur_move      = 0;
+    this._gameover      = false;
 
     // generate moves
     let x = this._max_level,
         y = this._pads.length - 1;
 
-    while(x--) {
+    while (x--) {
       this._moves.push(Math.round(Math.random() * y));
     }
   }
@@ -86,16 +84,24 @@ class Simon extends Game {
     // start button
     this._start_btn.on('click', (e) => {
       e.preventDefault();
-      if(!this._playing) {
+
+      // wait for game to finish before restarting
+      if (!this._playing) {
         this.play();
       }
     });
 
     // pads
     this._pads.on('click', (e) => {
-      if(!this._cycling) {
+
+      // don't process if cycling moves
+      if (!this._cycling) {
         this.pulse($(e.currentTarget));
-        this.update($(e.currentTarget).attr('padnum'));
+
+        // don't update if game has ended
+        if (!this._gameover) {
+          this.update($(e.currentTarget).attr('padnum'));
+        }
       }
     });
   }
@@ -104,6 +110,7 @@ class Simon extends Game {
   play() {
 
     this._playing = true;
+    this._cycling = true;
     this.init();
     this.cycle();
   }
@@ -113,27 +120,25 @@ class Simon extends Game {
 
     let x = this._level;
 
-    // disable input, queue user
-    this._cycling = true;
-
-    if(this._level > 1) {
+    // queue the user
+    if (this._level > 1) {
       this.display('level ' + this._level);
     } else {
       this.display('ready?', this._message_dur);
     }
 
     // start sequence
-    while(x--) {
+    while (x--) {
       setTimeout((y) => {
         this.pulse(this._pads.eq(this._moves[this._level - y]));
 
-        // last cycle » re-enable input, queue user
-        if(y === 1) {
-          this._cycling = false;
+        // last cycle » queue user, start timer
+        if (y === 1) {
           setTimeout(() => {
+            this._cycling = false;
             this.display('go!', this._message_dur * 2);
             this.timer(1);
-          }, this._message_dur);
+          }, this._message_dur / 2);
         }
       }, (this._level - x) * this._seqence_dur, x + 1);
     }
@@ -145,20 +150,19 @@ class Simon extends Game {
     let x = this._move_dur / 1000;
 
     // start timer
-    if(start) {
-      while(x) {
+    if (start) {
+      while (x) {
         this._timer[x - 1] = setTimeout((y) => { log('timer: ' + y);
-
-          // times up!
-          if(y === this._move_dur / 1000) {
+          if (y === this._move_dur / 1000) { // times up!
             this.lose('times up!');
+            this.timer(0);
           }
         }, x * 1000, x--);
       }
 
     // kill timer
     } else { log('kill timer');
-      while(x--) {
+      while (x--) {
         clearTimeout(this._timer[x]);
         delete(this._timer[x]);
       }
@@ -168,14 +172,12 @@ class Simon extends Game {
   // verify input/move
   update(pad) { log('update() : ' + parseInt(pad) + ', ' + this._moves[this._cur_move]);
 
-    if(this._playing) {
-
-      // kill timer
-      this.timer(0);
+    if (this._playing) {
+      this.timer(0); // kill timer
 
       // correct move
-      if(parseInt(pad) === this._moves[this._cur_move]) {
-        this._cur_move = this._cur_move + 1;
+      if (parseInt(pad) === this._moves[this._cur_move]) {
+        this._cur_move += 1;
         this.next();
 
       // incorrect move
@@ -189,14 +191,15 @@ class Simon extends Game {
   next() { log('next() : ' + this._cur_move + ', ' + this._level);
 
     // level complete
-    if(this._cur_move === this._level) {
+    if (this._cur_move === this._level) {
       this._cur_move = 0;
+      this._cycling = true;
       this.level(this._level + 1);
       setTimeout(() => {
         this.cycle();
       }, this._message_dur);
 
-    // reset timer
+    // restart timer
     } else {
       this.timer(1);
     }
@@ -205,12 +208,14 @@ class Simon extends Game {
   // game over
   lose(msg) { log('lose()');
 
-    this._playing = false;
-    this.timer(0);
+    // disable input, display message
+    this._gameover = true;
     this.display(msg, this._message_dur * 2);
 
+    // display game-over outro
     setTimeout(() => {
       this.display('game over', this._message_dur * 3);
+      this._playing = false;
     }, this._message_dur * 2);
   }
 
@@ -219,7 +224,6 @@ class Simon extends Game {
 
     this._level = level;
   }
-
 
   // trigger flash
   pulse(el) {
@@ -234,14 +238,14 @@ class Simon extends Game {
   display(msg, dur) {
 
     // set/clear text
-    if(msg) {
+    if (msg) {
       this._display.html(msg);
     } else {
       this._display.html('&nbsp;');
     }
 
     // clear after duration
-    if(dur) {
+    if (dur) {
       setTimeout(() => {
         this.display(0);
       }, dur);
